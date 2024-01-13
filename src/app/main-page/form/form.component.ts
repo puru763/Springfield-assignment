fd86944eadf25ede1bc87b847f48391b45649f72
Purvesh Krishnani <purvesh152001@gmail.com>
2024-01-14 03:37:42 +0530

    form UI and integration

--- src/app/main-page/form/form.component.ts
+++ src/app/main-page/form/form.component.ts
@@ -1,12 +1,35 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { ServiceService } from '../../service.service';
+import { HttpClientModule } from '@angular/common/http';
 @Component({
   selector: 'app-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, HttpClientModule],
+  providers: [ServiceService],
   templateUrl: './form.component.html',
   styleUrl: './form.component.scss'
 })
 export class FormComponent {
 
+  public pinCode!: any
+  public city: string = ''
+  public state: string = ''
+  public invalidData: boolean = false
+
+  constructor(private service: ServiceService) { }
+  public fetchLocation(): void {
+    let regex = new RegExp(/^[1-9]{1}[0-9]{2}\s{0,1}[0-9]{3}$/)
+    if (regex.test(this.pinCode)) {
+      this.invalidData = false
+      this.service.getLocationData(this.pinCode).subscribe({
+        next: (response: Array<{ City: string, State: string }>) => {
+          this.city = response[0].City
+          this.state = response[0].State
+        }
+      })
+    } else {
+      this.invalidData = true
+    }
+  }
 }
--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-
+import { FormComponent } from "./form/form.component";
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [],
+  imports: [FormComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
