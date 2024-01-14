979aa0d34c1bcccb825f2025112f9b4536a1114b
Purvesh Krishnani <purvesh152001@gmail.com>
2024-01-14 23:15:30 +0530

    Price section UI done

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { FormComponent } from "./form/form.component";
+import { NavbarComponent } from "./../navbar/navbar.component";
+import { PricingComponent } from "./pricing/pricing.component";
+
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [FormComponent],
+  imports: [FormComponent,NavbarComponent,PricingComponent],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.scss'
 })
--- src/app/main-page/pricing/pricing.component.ts
+++ src/app/main-page/pricing/pricing.component.ts
@@ -1,12 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-pricing',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './pricing.component.html',
   styleUrl: './pricing.component.scss'
 })
 export class PricingComponent {
 
+  public currentType = 'All'
+
+  public setActive(type: string) {
+    this.currentType = type
+  }
 }
